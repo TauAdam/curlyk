0f5165076f70c16e370508bef66162b22616985b
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-07-04 18:42:47 +0500

    chore: Refactor

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,12 +4,12 @@ import { useState } from 'react'
 import { MovieInfo } from '../types'
 import { Movie } from './movie'
 
-const my_api_key = import.meta.env.VITE_API_KEY
+const apiKey = import.meta.env.VITE_API_KEY
 
 export const App = () => {
   const [page, setPage] = useState<number>(1)
 
-  const topMoviesApi = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${my_api_key}&page=${page}`
+  const topMoviesApi = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`
 
   const getMovies = async () => {
     return await axios.get(topMoviesApi).then(res => res.data.results)
